684501d8946ef4a29598972bb57372ba0393aa3a
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2019-11-13 16:03:29 +0100

    axios interceptor with function for remove the interceptor

--- src/index.js
+++ src/index.js
@@ -6,22 +6,26 @@ import axios from 'axios';
 import registerServiceWorker from './registerServiceWorker';
 
 
-axios.interceptors.request.use(request => {
+const myInterceptor_Request = axios.interceptors.request.use(request => {
     console.log('[index.js request] ',request);
     return request;
 }, error => {
     console.log('[index.js request error] ',error);
     return Promise.reject(error);
 });
+// For remove the interceptor
+//axios.interceptors.request.eject(myInterceptor_Request);
 
-axios.interceptors.response.use(response => {
+
+const myInterceptor_Response = axios.interceptors.response.use(response => {
     console.log('[index.js response] ',response);
     return response;
 }, error => {
     console.log('[index.js response error] ',error);
     return Promise.reject(error);
-})
-  
+});
+// For remove the interceptor
+//axios.interceptors.request.eject(myInterceptor_Response);
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
